92a01408bbf790a1b09838443447d83c0f3ae7e7
Pietro <pietro.florian@gmail.com>
2026-09-01 00:55:36 -0500

    nueva api

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbxMm4fExrDZM9X3A6NNoPH3dWTlUs2UQDU8aqvpMQQEBSgYQmYdcL5ZecDlUYtJtY25/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbyJffKPdCbyWQGftl_Ay7VK28yQM5iJE1FMN4b97_yxCUvteTWzl5e1L4xmYtDxUz8/exec";
 
 function escapeHTML(str) {
   if (str === null || str === undefined) return '';
